465d3971eb67343c5ddad4d30ce1b1d9a07f0441
sorin-bogde <sbogde@gmail.com>
2024-06-22 10:57:05 +0100

    introduced EfficientNetB7

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -60,13 +60,14 @@ const UploadForm = ({ onResultsUpdate }) => {
             <MenuItem value="densenet121">DenseNet121</MenuItem>
             <MenuItem value="mobilenet">MobileNet</MenuItem>
             <MenuItem value="efficientnetb0">EfficientNetB0</MenuItem>
-            <MenuItem value="efficientnetb1">EfficientNetB1</MenuItem>
+            {/* <MenuItem value="efficientnetb1">EfficientNetB1</MenuItem> */}
             <MenuItem value="efficientnetb2">EfficientNetB2</MenuItem>
-            <MenuItem value="efficientnetb3">EfficientNetB3</MenuItem>
+            {/* <MenuItem value="efficientnetb3">EfficientNetB3</MenuItem> */}
             <MenuItem value="efficientnetb4">EfficientNetB4</MenuItem>
-            <MenuItem value="inceptionv3">InceptionV3</MenuItem>
-            <MenuItem value="xception">Xception</MenuItem>
-            <MenuItem value="vgg16">VGG16</MenuItem>
+            {/* <MenuItem value="inceptionv3">InceptionV3</MenuItem> */}
+            {/* <MenuItem value="xception">Xception</MenuItem> */}
+            {/* <MenuItem value="vgg16">VGG16</MenuItem> */}
+            <MenuItem value="efficientnetb7">EfficientNetB7</MenuItem>
             <MenuItem value="vgg19">VGG19</MenuItem>
           </Select>
         </FormControl>
